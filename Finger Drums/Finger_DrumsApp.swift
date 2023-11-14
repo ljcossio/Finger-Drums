e82564b0615e809701af6ae8a814001735f23971
ljcossio <79971098+ljcossio@users.noreply.github.com>
2023-11-14 16:49:50 +0100

    Initial Commit

---
+++ Finger Drums/Finger_DrumsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finger_DrumsApp.swift
+//  Finger Drums
+//
+//  Created by Luis Cossío on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Finger_DrumsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
